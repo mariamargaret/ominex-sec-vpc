11de259811ffa8fa40f0eb11efeb83c32fa7f944
mariamargaret <44648153+mariamargaret@users.noreply.github.com>
2024-09-09 12:06:42 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -13,4 +13,4 @@ output "service_name" {
   value = aws_vpc_endpoint_service.sec-gwlb_endpoint_service.service_name
 }
 output "Bastion_ip" {
-  value = aws_instance.app_server.public_ip
+  value = aws_instance.app_server.public_ip.id
